b21cef3049668a5b8caf56b08ed10c2059a73c85
guoquan <guoquan_731296441@qq.com>
2022-08-08 23:27:38 +0800

    初始化项目，初始化页面布局结构，完成侧边栏的布局和样式

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '@/views/Home.vue'
+
+Vue.use(VueRouter)
+const routes = [
+    {
+        path: '/',
+        component: Home,
+        // name: 'Home'
+    }
+]
+
+const router = new VueRouter({
+    node: 'history',
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="auto">
+            <!-- 侧边栏组件 -->
+            <common-aside></common-aside>
+        </el-aside>
+        <el-container>
+            <!-- 头部区域 -->
+            <el-header>Header</el-header>
+            <!-- 主内容区域 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+import commonAside from '@/common/commonAside.vue'
+
+
+
+export default {
+    dtat() {
+        return {
+
+        }
+    },
+   components: {
+    commonAside
+   }
+}
+</script>
+
+<style lang="less" scoped>
+    .el-container {
+        height: 100%;
+    }
+
+    .el-aside {
+        height: 100%;
+    }
+
+    .el-header {
+        background-color: black;
+        color: white
+    }
+
+    .el-main {
+        padding-top: 0;
+    }
+</style>
