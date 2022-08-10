1f49bc5f81b1cb06016421f3bc890ee1f4cf5a6a
guoquan <guoquan_731296441@qq.com>
2022-08-10 18:36:06 +0800

    组件订单展示

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -35,7 +35,18 @@
       </el-col>
 
       <!-- 第二列 -->
-      
+      <el-col :span="16">
+        <div class="num">
+          <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex', padding: 0 }">
+            <i class="icon" :class="'el-icon-' + item.icon" :style="{ background: item.color }"></i>
+            <div class="detail">
+              <p class="num">{{ item.value }}</p>
+              <p class="txt">{{ item.name }}</p>
+            </div>
+          </el-card>
+        </div>
+      </el-col>
+
     </el-row>
   </el-row>
     
@@ -89,7 +100,45 @@ export default {
         todayBuy: '今日购买',
         monthBuy: '本月购买',
         totalBuy: '总购买'
-      }
+      },
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
     }
   }
 }
